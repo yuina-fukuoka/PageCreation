d0c7ec0c38fc92dc31e44aab874315bd93e7460a
yuina.fukuoka <yuina.fukuoka@rakuten.com>
2018-06-15 05:59:42 +0000

    UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Page Creation</title>
+        
+         <!-- Bootstrap CSS-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Bootstrap JavaScript-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -47,6 +54,7 @@
 
             .title {
                 font-size: 84px;
+                text-align: center;
             }
 
             .links > a {
@@ -62,10 +70,41 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            
+            
+            /*オリジナル効果*/
+            .result {
+                background-image: url("/background.jpg");
+                border: solid;
+                width: 600px;
+                height: 400px;
+                margin: 50px;
+                float: right;
+            }
+            
+            
+            .content {
+                color: #000;
+                font-size: 30px;
+                margin:10px;
+                width:300;
+                height:200;
+                border-radius: 100px;
+            }
+            
+            .wrapper {
+                margin: 30px;
+                float: right;
+            }
+            
+            .sidevar {
+                margin: 30px;
+                float: left;
+            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+<!--        <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -91,5 +130,48 @@
                 </div>
             </div>
         </div>
+-->
+
+    <header>
+            
+        </header>
+        <div>
+            <div class="title">
+                Page Creation
+            </div>
+        </div>
+        
+        <aside class="sidevar">
+            <section>
+            <p>page title</p>
+            <input type="text" name="title_color_1">
+            <!--ここにコンソール（テキストエリア）を表示-->
+            </section>
+            <section>
+            <p>background image</p>
+            <button>sea</button><!--Formつける-->
+            <button>sky</button>
+            <button>flower</button>
+            <br>
+            <button>star</button><!--Formつける-->
+            <button>rainbow</button>
+            <button>cafe</button>
+            </section>
+        </aside>
+        
+        <div class="wrapper">
+            <div class="result">
+            <figure class="content">
+                <img src="/cupcake.jpg" alt="mainimage">   
+                <figcaption>recommendation</figcaption>
+            </figure>
+            </div>
+        </div>
+        <footer>
+            
+        </footer>
+    
+    
     </body>
+
 </html>
